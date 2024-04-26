e513b252c7ab213a2836019a26315a1cf62dd214
ogdenqstu13 <ogdenqstu13@gmail.com>
2024-04-26 00:26:22 -0400

    Added hero section and changed picture to me

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Quinn</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 7+ years of experience using various JS frameworks.
+          I also work in livestream production part time. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:ogdenqstu13@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
